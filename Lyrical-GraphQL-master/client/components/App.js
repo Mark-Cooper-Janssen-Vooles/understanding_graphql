98ddfd6dcbb9fa619b0517b5413f3922b24b9051
mark janssen <mark.janssen-vooles@xero.com>
2022-02-18 12:26:56 +1100

    Attempted to add react-router but it didn't work, ignore it for now and find workarounds.. dependency hell looms trying to upgrade to fix it

---
+++ Lyrical-GraphQL-master/client/components/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default ({ children }) => {
+  console.log('test')
+  return <div className="container">{children}</div>;
+};
--- Lyrical-GraphQL-master/client/index.js
+++ Lyrical-GraphQL-master/client/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { Router, Route, hashHistroy, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import SongList from './components/SongList';
+import App from './components/App';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
+    // <ApolloProvider client={client}>
+    //   <Router history={hashHistroy}>
+    //     <Route path="/" component={App}>
+    //       <IndexRoute component={SongList} />
+    //     </Route>
+    //   </Router>
+    // </ApolloProvider>
+
     <ApolloProvider client={client}>
       <SongList />
     </ApolloProvider>
